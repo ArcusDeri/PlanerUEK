245457561997466d4a6fbe4b4e0ace1efef2a865
MARCIN\Marcin <michniak@yandex.com>
2017-11-19 20:31:19 +0100

    Add NuGet packages for HTML agility pack and Google Calendar Api. Create api key and add to root directory (client_secret.json).

--- PlanerUEK/Program.cs
+++ PlanerUEK/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 
 namespace PlanerUEK
 {
