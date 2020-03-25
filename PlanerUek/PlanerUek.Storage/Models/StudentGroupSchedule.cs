aef4e46ca70cd427cfe993c2b171730954c948c8
Marcin Michniak <marcin.michniak@fortedigital.no>
2020-03-25 21:38:49 +0100

    Deserialize xml to c# model

--- PlanerUek/PlanerUek.Storage/Models/StudentGroupSchedule.cs
+++ PlanerUek/PlanerUek.Storage/Models/StudentGroupSchedule.cs
@@ -1,9 +1,91 @@
-﻿namespace PlanerUek.Storage.Models
-{
-    public class StudentGroupSchedule
-    {
-        public string GroupName { get; set; }
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
 
-        public string GroupId { get; set; }
+namespace PlanerUek.Storage.Models
+{
+    [XmlRoot(ElementName="plan-zajec")]
+    public class StudentGroupSchedule {
+        [XmlElement(ElementName="okres")]
+        public List<Period> Periods { get; set; }
+        
+        [XmlElement(ElementName="zajecia")]
+        public List<ScheduleClass> ScheduleClasses { get; set; }
+        
+        [XmlAttribute(AttributeName="typ")]
+        public string Type { get; set; }
+        
+        [XmlAttribute(AttributeName="id")]
+        public string Id { get; set; }
+        
+        [XmlAttribute(AttributeName="nazwa")]
+        public string Name { get; set; }
+        
+        [XmlAttribute(AttributeName="od")]
+        public DateTime FromDate { get; set; }
+        
+        [XmlAttribute(AttributeName="do")]
+        public DateTime ToDate { get; set; }
+    }
+
+    [XmlRoot(ElementName="okres")]
+    public class Period {
+        [XmlAttribute(AttributeName="od")]
+        public DateTime From { get; set; }
+        
+        [XmlAttribute(AttributeName="do")]
+        public DateTime To { get; set; }
+        
+        [XmlAttribute(AttributeName="nazwa")]
+        public string Name { get; set; }
+        
+        [XmlAttribute(AttributeName="wybrany")]
+        public string Chosen { get; set; }
+    }
+
+    [XmlRoot(ElementName="zajecia")]
+    public class ScheduleClass
+    {
+        private string _toHour;
+        
+        [XmlElement(ElementName="termin")]
+        public DateTime Date { get; set; }
+        
+        [XmlElement(ElementName="dzien")]
+        public string WeekDay { get; set; }
+        
+        [XmlElement(ElementName="od-godz")]
+        public string FromHour { get; set; }
+
+        [XmlElement(ElementName = "do-godz")]
+        public string ToHour
+        {
+            get => _toHour;
+            set => _toHour = value.Substring(0, 5);
+        }
+        
+        [XmlElement(ElementName="przedmiot")]
+        public string Subject { get; set; }
+        
+        [XmlElement(ElementName="typ")]
+        public string Type { get; set; }
+        
+        [XmlElement(ElementName="nauczyciel")]
+        public Teacher Teacher { get; set; }
+        
+        [XmlElement(ElementName="sala")]
+        public string ClassRoom { get; set; }
+        
+        [XmlElement(ElementName="uwagi")]
+        public string Notes { get; set; }
+    }
+
+    [XmlRoot(ElementName="nauczyciel")]
+    public class Teacher {
+        [XmlAttribute(AttributeName="moodle")]
+        public string Moodle { get; set; }
+        
+        [XmlText]
+        public string Text { get; set; }
     }
 }
--- PlanerUek/PlanerUek.Storage/Providers/StudentGroupScheduleProvider.cs
+++ PlanerUek/PlanerUek.Storage/Providers/StudentGroupScheduleProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Xml;
+using System.Xml.Serialization;
 using PlanerUek.Storage.Interfaces;
 using PlanerUek.Storage.Models;
 
@@ -19,8 +20,11 @@ namespace PlanerUek.Storage.Providers
             var xmlDocument = new XmlDocument();
             var endpoint = GetScheduleEndpoint(groupId);
             xmlDocument.Load(endpoint);
-            
-            return new StudentGroupSchedule();
+
+            using TextReader reader = new StringReader(xmlDocument.InnerXml);
+            var serializer = new XmlSerializer(typeof(StudentGroupSchedule));
+
+            return (StudentGroupSchedule) serializer.Deserialize(reader);
         }
 
         private string GetScheduleEndpoint(string groupId) => string.Format(_scheduleEndpointTemplate, groupId);
