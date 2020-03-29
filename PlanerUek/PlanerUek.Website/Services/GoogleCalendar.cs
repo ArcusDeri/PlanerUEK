3f14016e4be5745f721656067f5404cbc06af5da
Marcin Michniak <marcin.michniak@fortedigital.no>
2020-03-29 14:12:02 +0200

    Add google calendar api package, map schedule class to google calendar event

--- PlanerUek/PlanerUek.Website/Services/GoogleCalendar.cs
+++ PlanerUek/PlanerUek.Website/Services/GoogleCalendar.cs
@@ -1,5 +1,9 @@
-﻿using PlanerUek.Storage.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PlanerUek.Storage.Models;
 using PlanerUek.Website.Models;
+using Google.Apis.Calendar.v3.Data;
 
 namespace PlanerUek.Website.Services
 {
@@ -7,7 +11,34 @@ namespace PlanerUek.Website.Services
     {
         public CalendarUpdateResult AddStudentGroupSchedule(StudentGroupSchedule schedule)
         {
+            var calendarEvents = ResolveEventsFromSchedule(schedule);
             throw new System.NotImplementedException();
+        }
+
+        private IEnumerable<Event> ResolveEventsFromSchedule(StudentGroupSchedule schedule)
+        {
+            var result = schedule.ScheduleClasses
+                .Where(w => !w.Type.ToLower().Contains("przeniesienie"))
+                .Select(x => new Event()
+                {
+                    Start = ResolveEventDate(x.Date, x.FromHour),
+                    End = ResolveEventDate(x.Date, x.ToHour),
+                    Summary = x.Subject,
+                    Location = x.ClassRoom,
+                    Description = $"{x.Type}, {x.Teacher.Text}",
+                    Reminders = new Event.RemindersData {UseDefault = false}
+                });
+            return result;
+        }
+
+        private EventDateTime ResolveEventDate(DateTime date, string hour)
+        {
+            var hourDate = DateTime.Parse(hour);
+            return new EventDateTime
+            {
+                TimeZone = "Europe/Warsaw",
+                DateTime = new DateTime(date.Year, date.Month, date.Day, hourDate.Hour, hourDate.Minute, 0)
+            };
         }
     }
 }
