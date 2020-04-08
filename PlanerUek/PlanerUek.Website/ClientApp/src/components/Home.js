4838d4b853a897cb200556bb22759258d8f7a7dc
Marcin Michniak <marcin.michniak@fortedigital.no>
2020-04-08 16:35:02 +0200

    Remove unused code, add description on frontend

--- PlanerUek/PlanerUek.Website/ClientApp/src/components/Home.js
+++ PlanerUek/PlanerUek.Website/ClientApp/src/components/Home.js
@@ -6,12 +6,20 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Hello, world!</h1>
-            <form method="POST" action="/api/StudentGroups/HandleTimetableForGroup">
-                <label htmlFor="groupName">Student group name</label>
-                <input type="text" name="groupName"/>
-                <input type="submit"/>
-            </form>
+        <p>
+            This application integrates your student group schedule with your Google Calendar. Type in your student group name
+            (the same you can find at <a href="http://planzajec.uek.krakow.pl/">planzajec.uek.krakow.pl</a>). The application
+            will map events in timetable to Google Calendar events and save them in your calendar.
+            
+            You will be prompted to sign into your Google account along the way. This application doesn't store any details about
+            your Google account.
+        </p>
+          <strong>Try submitting this group: KrDzIs3011Io</strong>
+        <form method="POST" action="/api/StudentGroups/HandleTimetableForGroup">
+            <label htmlFor="groupName">Student group name</label>
+            <input type="text" name="groupName"/>
+            <input type="submit"/>
+        </form>
       </div>
     );
   }
